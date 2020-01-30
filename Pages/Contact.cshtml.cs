f9426c480ee6e4ac2615e76dbac4f4c472e0ac77
chiccomotta <cristiano.motta@hotmail.it>
2020-01-30 18:04:04 +0100

    Added contact me page

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1
+{
+    // [Authorize]
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; } = "Contact me at cristiano.motta@hotmail.it";
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
